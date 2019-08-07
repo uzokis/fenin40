67bdf64ccd145f536a6392ae0c180fc1cb210127
Eli <eli@buzzard.bz>
2019-08-07 16:50:36 +0200

    generated some DB code + starting point

--- src/main/java/eu/fancybrackets/template/handler/ArduinoAPIHandler.java
+++ src/main/java/eu/fancybrackets/template/handler/ArduinoAPIHandler.java
@@ -1,33 +1,101 @@
 package eu.fancybrackets.template.handler;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.postgresql.jdbc.TimestampUtils;
 
+import eu.fancybrackets.template.jooq.generated.tables.pojos.Measurement;
+import eu.fancybrackets.template.jooq.generated.tables.records.MeasurementRecord;
 import eu.fancybrackets.template.verticle.ArduinoAPIVerticle;
+import eu.fancybrackets.template.verticle.MainVerticle;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import static eu.fancybrackets.template.jooq.generated.tables.Measurement.*;
 
 public class ArduinoAPIHandler {
 	private final static Logger LOG = Logger.getLogger(ArduinoAPIHandler.class.getName());
 
 	@Inject
-	private DSLContext jooq;
+	private Vertx vertx;
+
+	@Inject
+	private DataSource ds;
 
 	public void handleGet(RoutingContext context) {
-		try {
-			String containerid = context.pathParam(ArduinoAPIVerticle.API_PARAM_CONTAINER_ID);
-			LOG.fine(String.format("Handling request for %s", containerid));
-			JsonObject measurement = new JsonObject().put("value", "20").put("timestamp", new Date().toString());
-			JsonObject response = new JsonObject().put("measurement", measurement);
-			context.response().setChunked(true);
-			context.response().write(response.toString()).end();
-		} catch (Exception e) {
-			LOG.log(Level.SEVERE, "Unexpected exception", e);
-		}
+		vertx.executeBlocking(future -> {
+			try (Connection conn = ds.getConnection()) {
+				Measurement m = DSL.using(conn, SQLDialect.POSTGRES).selectFrom(MEASUREMENT)
+						.where(MEASUREMENT.CONTAINER_ID.eq(context.pathParam(ArduinoAPIVerticle.API_PARAM_CONTAINER_ID)))
+						.orderBy(MEASUREMENT.MEASURE_TIME.desc()).fetchAny().into(Measurement.class);
+
+				JsonObject measurement = new JsonObject().put("value", m.getValue()).put("timestamp", DateFormat.getDateTimeInstance().format(m.getMeasureTime()));
+				JsonObject response = new JsonObject().put("measurement", measurement);
+
+				future.complete(response);
+			} catch (SQLException e) {
+				future.fail(e);
+			}
+		}, future -> {
+			if (future.succeeded()) {
+				context.response().setChunked(true);
+				context.response().write(future.result().toString()).end();
+			} else {
+				LOG.log(Level.SEVERE, "Unexpected exception", future.cause());
+				context.response().setStatusCode(500).end();
+			}
+		});
+//		
+//		
+//		
+//		try {
+//			String containerid = context.pathParam(ArduinoAPIVerticle.API_PARAM_CONTAINER_ID);
+//			LOG.fine(String.format("Handling request for %s", containerid));
+//			JsonObject measurement = new JsonObject().put("value", "20").put("timestamp", new Date().toString());
+//			JsonObject response = new JsonObject().put("measurement", measurement);
+//			context.response().setChunked(true);
+//			context.response().write(response.toString()).end();
+//		} catch (Exception e) {
+//			LOG.log(Level.SEVERE, "Unexpected exception", e);
+//		}
+	}
+
+	public void handlePost(RoutingContext context) {
+		vertx.executeBlocking(future -> {
+			try (Connection conn = ds.getConnection()) {
+				DSL.using(conn).transaction(configuration -> {
+					LOG.info(String.format("**** Got %s", context.getBodyAsJson()));
+
+					Measurement m = new Measurement();
+					m.setContainerId(context.pathParam(ArduinoAPIVerticle.API_PARAM_CONTAINER_ID));
+					m.setValue(context.getBodyAsJson().getString("value"));
+					m.setMeasureTime(new Timestamp(new Date().getTime()));
+
+					MeasurementRecord mr = DSL.using(configuration).newRecord(MEASUREMENT, m);
+					mr.store();
+					future.complete();
+				});
+			} catch (SQLException e) {
+				future.fail(e);
+			}
+		}, future -> {
+			if (future.succeeded()) {
+				context.response().setStatusCode(201).end();
+			} else {
+				context.response().setStatusCode(500).end();
+			}
+		});
 	}
 }
--- src/main/java/eu/fancybrackets/template/verticle/ArduinoAPIVerticle.java
+++ src/main/java/eu/fancybrackets/template/verticle/ArduinoAPIVerticle.java
@@ -6,6 +6,7 @@ import eu.fancybrackets.template.handler.ArduinoAPIHandler;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class ArduinoAPIVerticle extends AbstractVerticle {
 	@Inject
@@ -19,7 +20,9 @@ public class ArduinoAPIVerticle extends AbstractVerticle {
 	@Override
 	public void start() throws Exception {
 		super.start();
+		this.router.route("/api/*").handler(BodyHandler.create());//allows for easy body parsing in the other handlers
 		this.router.route(String.format("/api/:%s",API_PARAM_CONTAINER_ID)).method(HttpMethod.GET).handler(arduinoAPIHandler::handleGet);
+		this.router.route(String.format("/api/:%s",API_PARAM_CONTAINER_ID)).method(HttpMethod.POST).handler(arduinoAPIHandler::handlePost);
 		//TODO add routes
 	}
 }
